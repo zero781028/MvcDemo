73ef26d19b4e94599ddbd34b5eb2ce93dfb842d8
Jerry-Wu <zero781028@nec.com.tw>
2015-10-30 17:20:33 +0800

    新增CDTBL、ExchangeDetailVeiwModel,WebAPI ExchangeDetailsController.cs

---
+++ Domain/ViewModels/ExchangeDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModels
+{
+    public class ExchangeDetailViewModel
+    {
+        public string MemberID { get; set; }
+        public string Txn_SN { get; set; }
+        public string ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int SN { get; set; }
+        public string Reason { get; set; }
+        public string Rank { get; set; }
+        public int Price { get; set; }
+        public int Qty { get; set; }
+    }
+}
---
+++ MvcDemo/API/ExchangeDetailsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL.Services;
+
+
+namespace MvcDemo.API
+{
+    public class ExchangeDetailsController : ApiController
+    {
+        private MemberExchangeService service;
+        public ExchangeDetailsController()
+        {
+            service = new MemberExchangeService();
+        }
+        // GET api/exchangedetails
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/exchangedetails/5
+        public HttpResponseMessage Get(string MemberID, string Txn_SN)
+        {
+            try
+            {
+                var datas = service.GetExchangeDetails(MemberID, Txn_SN);
+                return Request.CreateResponse(HttpStatusCode.OK, datas);
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message.ToString());
+            }            
+        }
+
+        // POST api/exchangedetails
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/exchangedetails/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/exchangedetails/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
