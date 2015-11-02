b3cc57a774dd190cb2c660c038bb339253c0ad15
Jerry-Wu <jerry-wu@nec.com.tw>
2015-11-02 08:34:35 +0800

    新增CDTBL資料表、ExchangeDetailViewModel

--- Domain/ViewModels/ExchangeDetailViewModel.cs
+++ Domain/ViewModels/ExchangeDetailViewModel.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,26 +19,3 @@ namespace Domain.ViewModels
         public int Qty { get; set; }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.ViewModels
-{
-    public class ExchangeDetailViewModel
-    {
-        public string MemberID { get; set; }
-        public string Txn_SN { get; set; }
-        public string ItemID { get; set; }
-        public string ItemName { get; set; }
-        public int SN { get; set; }
-        public string Reason { get; set; }
-        public string Rank { get; set; }
-        public int Price { get; set; }
-        public int Qty { get; set; }
-    }
-}
->>>>>>> issue1
