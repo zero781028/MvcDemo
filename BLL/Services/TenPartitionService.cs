a0bdf1d388204c933dd89dcafdeea7991145ba2e
Jerry-Wu <jerry-wu@nec.com.tw>
2015-11-06 10:07:12 +0800

    新增 TenPartitionService

---
+++ BLL/Services/TenPartitionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Domain.ViewModels;
+
+namespace BLL.Services
+{
+    public class TenPartitionService
+    {
+        internal COSMED_CRMEntities db;
+        public TenPartitionService()
+        {
+            db = new COSMED_CRMEntities();
+        }
+    }
+}
