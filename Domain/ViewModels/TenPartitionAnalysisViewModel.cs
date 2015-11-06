b46f5c1e1afcbdf35a2a688a921a91979f0a54f6
Jerry-Wu <jerry-wu@nec.com.tw>
2015-11-06 10:04:01 +0800

    新增 COSMED_CRM Entity, TenPartitionAnalysisViewModel

---
+++ Domain/ViewModels/TenPartitionAnalysisViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModels
+{
+    public class TenPartitionAnalysisViewModel
+    {
+        public int Price { get; set; }
+        public float TTotalPrice { get; set; }
+        public float TTotalCnt { get; set; }
+        public float TTotalQty { get; set; }
+    }
+}
