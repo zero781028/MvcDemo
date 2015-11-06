da19202d6573b8cf42cf05e67b9b5e41b0e88c07
Jerry-Wu <jerry-wu@nec.com.tw>
2015-11-06 11:23:49 +0800

    新增 TenPartition 的 Controller和View

---
+++ MvcDemo/Controllers/TenPartitionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class TenPartitionController : Controller
+    {
+        public ActionResult TenPartitionAnalysis()
+        {
+            return View();
+        }
+    }
+}
